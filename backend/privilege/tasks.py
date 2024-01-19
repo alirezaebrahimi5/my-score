f3a92a2c1a427af03a6ad688cb59477b88eeeac1
sooshiance <s.katafract@gmail.com>
2024-01-19 22:03:41 +0500

    add some details for task in privilege app

--- backend/privilege/tasks.py
+++ backend/privilege/tasks.py
@@ -1,9 +1,18 @@
 from celery import shared_task
+from datetime import time
 from django.utils.timezone import get_current_timezone
 
 from .models import UserTime, ScoreComputation, UserGivenScore
 
+from django.conf import settings
+
+
+User = settings.AUTH_USER_MODEL
+
 
 @shared_task
 def caculate_whole_time():
-    return 
+    if get_current_timezone() == time(hour=23, minute=0, second=0):
+        k = 0
+        for user in User.objects.all():
+            pass
